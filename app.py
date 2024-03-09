42c70ea3d365c44d9c8916d2b0acbecc1f6b463f
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-03-09 01:00:12 -0500

    Created a recent files section. Can delete uploaded files.

--- app.py
+++ app.py
@@ -22,20 +22,45 @@ app.config['csv'] = filetype_paths['csv']
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        if 'loggerfile' in request.files:
+        if 'clear-recent' in request.form:
+            with open("static/cache/recent.txt", 'w') as delete_data:
+                delete_data.close()
+            for filetype in filetype_paths:
+                for file in os.listdir(filetype_paths[filetype]):
+                    os.remove(os.path.join(filetype_paths[filetype], file))
+
+        elif 'loggerfile' in request.files:
             file = request.files['loggerfile']
             filename = file.filename
             filetype = filename[filename.find(".")+1:]
             file.save(os.path.join(app.config[filetype], filename))
+
+            with open("static/cache/recent.txt", 'a+') as save_data:
+                save_data.write(filename+'\n')
+                save_data.close()
+
             return redirect(url_for('view_log', file=filename[:filename.find(".")], type=filetype))
         else:
             message = request.form['log-text-field']
             return message
-    return render_template('index.html')
+
+    # Debug Needed    # # #
+    recent, recent_files = False, None
+    storage = []
+    with open("static/cache/recent.txt", 'r', encoding='utf-8') as read_data:
+        if read_data.readlines():
+            recent = True
+            for line in read_data.readlines():
+                storage.append(line)
+            recent_files = str(storage)
+
+    return render_template('index.html', turnOn=recent, recent_files=recent_files)
+    # Debug Needed    # # #
 
 @app.route('/loggersession/<file>.<type>', methods=['GET'])
 def view_log(file, type:str):
     filename = f"{file}.{type}"
+
     with open(f"{filetype_paths[type]}/{filename}", 'r', encoding='utf-8') as opened_file:
         lines = opened_file.readlines()
         return render_template('logger-session.html', lines=lines)
