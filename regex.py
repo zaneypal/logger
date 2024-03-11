789e660d9e6fa76057d1c2d7d769f13b832c6525
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-03-10 20:26:56 -0400

    Finished regex function to detect all matches of a query in a log row.

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = {
+patterns = {
     'ip_address': re.compile(r"^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$"),
     'hostname': re.compile(r"^(?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?)*\.?$") 
 }
@@ -19,6 +19,30 @@ def regex(pattern, text, exact=True):
         else:
             print("No match found")
 
+
+message = 'word...this word is a word'
+pattern = re.compile("word")
+
+indexes = []
+def multiregex(pattern, string, index_offset=0):
+    try:
+        result = str(re.search(pattern,string))
+        #print(f"result: {result}\n")
+        index = (int(result[result.find("(")+1:result.find(",")])+index_offset,int(result[result.find(",")+2:result.find(")")])+index_offset)
+        #print(f"index: {index}\n")
+        indexes.append(index)
+        #print(f"indexes: {indexes}\n")
+
+        current_endpoint = indexes[len(indexes)-1][1]-index_offset
+        #print(f"current_endpoint: {current_endpoint}\n")
+        new_string = string[current_endpoint:]
+        #print(f"new_string: {new_string}\n")
+        multiregex(pattern, new_string, current_endpoint)
+    except ValueError:
+        pass
+    
+multiregex(pattern, message)
+print(indexes)
 
 #regex(pattern['ip_address'], "192.168.1.55")
 #regex(pattern['ip_address'], "funny")
