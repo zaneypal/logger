0a3ce7b57f1de82e476a1a1142494dac891cf0c3
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-03-12 22:13:35 -0400

    Fixed regex function so that it works better.

--- regex.py
+++ regex.py
@@ -6,45 +6,46 @@ patterns = {
     'hostname': re.compile(r"^(?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?)*\.?$") 
 }
 
-message = 'word...this word is a word'
-message2= "not a word, so no, not a word."
-pattern = re.compile("word")
-indexes = []
+message2 = """random message
+in here mtttts
+mmmhhusssmsyuuh
+283628:)!"""
+pattern = r"m.{4}s"
 
-def multiregex(pattern, string, indexes, index_offset=0, new_session=True, inverse=False):
+def multiregex(pattern, msg, indexes, index_offset=0, new_session=True):
     try:
         if new_session == True:
             indexes = []
-            
-        result = str(re.search(pattern,string))
+        result = str(re.search(pattern,msg))
         index = (int(result[result.find("(")+1:result.find(",")])+index_offset,int(result[result.find(",")+2:result.find(")")])+index_offset)
         indexes.append(index)
 
         current_endpoint = indexes[len(indexes)-1][1]
-        new_string = string[current_endpoint-index_offset:]
+        new_string = msg[current_endpoint-index_offset:]
         multiregex(pattern, new_string, indexes, current_endpoint, new_session=False)
     except ValueError:
         pass
-
-    if inverse == True:
-        inverse_indexes = []
-        counter = 0
-        for index in indexes:
-            if counter == 0: 
-                if index[0] != 0:
-                    inverse_indexes.append((0, index[0]))
-                else:
-                    inverse_indexes.append((indexes[counter][1], indexes[counter+1][0]))
-            else:
-                inverse_indexes.append((indexes[counter-1][1], indexes[counter][0]))
-            if (indexes[-1][1] < len(string)) and index == indexes[-1]:
-                inverse_indexes.append((indexes[counter][1], len(string)))
-            counter += 1
-        indexes = inverse_indexes
     return indexes
 
-print(multiregex(pattern, message2, indexes))
-print(multiregex(pattern, message2, indexes, inverse=True))
+indexes = multiregex(pattern, message2, indexes=[])
+message = list(message2)
 
-#for index in multiregex(pattern, message, indexes):
-#    print(message[index[0]:index[1]])
+def html_insert(start, end, list, tag):
+    if tag == "br":
+        for ch in "<br />"[::-1]:
+            list.insert(end, ch)
+    else:
+        for ch in f"</{tag}>"[::-1]:
+            list.insert(end, ch)
+        for ch in f"<{tag}>"[::-1]:
+            list.insert(start, ch)
+
+for start, end in reversed(indexes):
+    html_insert(start, end, message, "mark")
+
+new_string = "".join(message)
+
+for start, end in reversed(multiregex("\n", new_string, indexes=[])):
+    html_insert(start, end, message, "br")
+
+print(newer_string := "".join(message))
