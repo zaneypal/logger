ca7322ccd6e80277696d9b51cdc129c00ceefd89
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-03-20 14:02:18 -0400

    File upload now shows name of file. Upload button is now more interactive

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,12 @@ function openSecondBar() {
     }
 }
 
+const loggerFile = document.getElementById('loggerfile');
+const selectedFileName = document.getElementById('selected-filename')
+
+loggerFile.addEventListener('change', function() {
+    selectedFileName.textContent = this.files[0].name;
+    document.getElementById('upload-button').classList.add('ready');
+})
+
+console.log("Hi " + event.target.files[0].name);
