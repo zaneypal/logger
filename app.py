e1be243fee849d2b80e1122ed0251ce1979f8e7a
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-04-13 15:56:49 -0400

    App no longer depends on directory-saved files to access files, relies on DB only.

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import and_, or_
 from markupsafe import Markup
 from regex import multiregex, html_insert, patterns
-import os, csv, re, json
+import os, csv, re, json, datetime
+
+def get_exact_datetime():
+    date = str(datetime.datetime.now()).replace("-", "").replace(":", "").replace(" ", "")
+    return date[:date.find(".")]
 
 class Base(DeclarativeBase):
     pass
@@ -16,43 +21,21 @@ db.init_app(app)
 
 class recentFile(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True, nullable=False)
-    content: Mapped[str] = mapped_column(unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
+    upload_date: Mapped[str] = mapped_column(nullable=False)
 
 with app.app_context():
     db.create_all() 
 
 app.app_context().push()
 
-# Stores the pathnames of the directories that hold user-uploaded files
-filetype_paths = {
-    'txt': None,
-    'csv': None
-}
-
-# Locates the paths of needed directories from the static folder
-for filetype in filetype_paths:
-    for dir in os.listdir("static"):
-        if dir.__contains__(filetype):
-            filetype_paths[filetype] = f"static/{dir}"
-
-# Applying the configurations made necessary by Flask
-app.config['txt'] = filetype_paths['txt']
-app.config['csv'] = filetype_paths['csv']
-
 # Redirects user to homepage when visting the site
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        # Deletes all recently uploaded files from history
+        # Deletes all recently uploaded files from database
         if 'clear-recent' in request.form:
-            with open("static/cache/recent.txt", 'w') as delete_data:
-                delete_data.close()
-
-            for filetype in filetype_paths:
-                for file in os.listdir(filetype_paths[filetype]):
-                    os.remove(os.path.join(filetype_paths[filetype], file))
-
             recentFile.query.delete()
             db.session.commit()
 
@@ -61,65 +44,52 @@ def index():
             if 'loggerfile' in request.files:
                 file = request.files['loggerfile']
                 filename = file.filename
-                filetype = filename[filename.find(".")+1:]
-                abs_filepath = f"{filetype_paths[filetype]}/{filename}"
-                file.save(os.path.join(app.config[filetype], filename))
-                with open(abs_filepath, 'r', encoding='utf-8') as opened_file:
-                    data = opened_file.read()
-                    db.session.add(recentFile(name=filename, content=data))
-                    db.session.commit()
-
-                with open("static/cache/recent.txt", 'a+') as save_data:
-                    save_data.write(filename+'\n')
+                data = file.read()
 
             # Allows user to also upload logs by pasting text
             else:
-                message = request.form['log-text-field']
-                with open("static/txt-files/newfile.txt", 'w+') as save_data:
-                    save_data.write(message)
-                filename='newfile.txt'
-                filetype='txt'
+                data = request.form['log-text-field'].encode()
+                filename = 'Pasted Log'
             
-            return redirect(url_for('view_log', file=filename[:filename.find(".")], type=filetype))
+            current_datetime = get_exact_datetime()
+            db.session.add(recentFile(name=filename, content=data, upload_date=current_datetime))
+            db.session.commit()
+            tag = db.session.execute(db.select(recentFile.upload_date).where(recentFile.upload_date == current_datetime)).scalar()
+
+            return redirect(url_for('view_log', file=filename, tag=tag))
 
     # Shows recently uploaded files
     recent = False
     recent_files = None
-    with open("static/cache/recent.txt", 'r+', encoding='utf-8') as read_data:
-        if len(read_data.readlines()) > 0:
-            recent = True
-            recent_files = db.session.execute(db.select(recentFile)).scalars()
-
+    if db.session.query(recentFile).count() > 0:
+        recent = True
+        recent_files = db.session.execute(db.select(recentFile)).scalars()
     return render_template('index.html', turnOn=recent, recent_files=recent_files)
 
-# Lets user view uploaded file on page
-@app.route('/loggersession/<file>.<type>', methods=['GET', 'POST'])
-def view_log(file, type:str):
-    filename = f"{file}.{type}"
-    abs_filepath = f"{filetype_paths[type]}/{filename}"
 
+# Lets user view uploaded file on page
+@app.route('/loggersession/<file>?tag=<tag>', methods=['GET', 'POST'])
+def view_log(file, tag):
     if request.method == 'POST':
         if 'regex-query' in request.form:
             pattern = request.form['regex-query']
-            return redirect(url_for('query_log', file=filename, pattern=pattern))
+            return redirect(url_for('query_log', file=file, tag=tag, pattern=pattern))
     else:
-        with open(abs_filepath, 'r', encoding='utf-8') as opened_file:
-            lines = opened_file.readlines()
-            return render_template('logger-session.html', lines=lines)
+        result_row = db.session.execute(db.select(recentFile).where(and_(recentFile.name == file, recentFile.upload_date == tag))).scalar()
+        logs = result_row.content.decode().split('\n')
+        return render_template('logger-session.html', logs=logs)
 
 # Lets user find regex matches of uploaded log file
-@app.route('/loggerquery/<file>/query=<pattern>', methods=['GET', 'POST'])
-def query_log(file, pattern):
-    type = file[file.find(".")+1:]
-    abs_filepath = f"{filetype_paths[type]}/{file}"
-
+@app.route('/loggerquery/<file>?tag=<tag>?query=<pattern>', methods=['GET', 'POST'])
+def query_log(file, tag, pattern):
     if request.method == 'POST':
         if 'regex-query' in request.form:
             pattern = request.form['regex-query']
-            return redirect(url_for('query_log', file=file, pattern=pattern))
+            return redirect(url_for('query_log', file=file, tag=tag, pattern=pattern))
 
-    with open(abs_filepath, 'r', encoding='utf-8') as read_data:
-        log = str(read_data.read())
+    else:
+        result_row = db.session.execute(db.select(recentFile).where(and_(recentFile.name == file, recentFile.upload_date == tag))).scalar()
+        log = result_row.content.decode()
         indexes = multiregex(pattern, log)
         result = html_insert(html_insert(log, indexes, "mark"), multiregex("\n", html_insert(log, indexes, "mark")), "br")
         result = Markup(result)
