2c71f3bcdc39260fe9d6e63310617ca6b31e0508
zaneypal <101521294+zaneypal@users.noreply.github.com>
2024-04-14 17:58:34 -0400

    CSS on secondary bar options and added more auto-field regex patterns.

--- regex.py
+++ regex.py
@@ -30,22 +30,22 @@ def format_date(date_str):
             break
     if date == False:
         date = None
-    return date
+    return f"{date.year}-{date.month}-{date.day}"
 
 patterns = {
     'hostname': [1, re.compile(r"(?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?)*\.?")],
-    'username': [1, re.compile(r"")],
+    'username': [1, re.compile(r"[a-z]{4,}\w+")],
     'ip_address': [1, re.compile(r"(?:25[0-5]|(?:2[0-4]|1[0-9]|[1-9]|)[0-9]\.){3}(?:25[0-5]|(?:2[0-4]|1[0-9]|[1-9]|)[0-9])")],
     'date': [2, re.compile(r"(?:[JFMASOND][a-z]{2,8} (?:(?:30|31)|[0-2]?[0-9]), \d{4})|(?:(?:\d{1,2}(?:\/|\.|-| |)){2}\d{2,4})")],
-    'time': [1, re.compile(r"")],
-    'request': [1, re.compile(r"")],
+    'time': [1, re.compile(r"(?:(?:1[0-2]|0?[0-9])(?::[0-5][0-9]){1,2} ?(?:AM|PM)?)|(?:(?:2[0-3]|1[0-9]|0?[0-9])(?::[0-5][0-9]){1,2})", re.IGNORECASE)],
+    'request': [1, re.compile(r"(?:GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)", re.IGNORECASE)],
     'command': [1, re.compile(r"")],
-    'protocol': [1, re.compile(r"")],
-    'status_code': [1, re.compile(r"")],
-    'data_in': [1, re.compile(r"")],
-    'data_out': [1, re.compile(r"")],
-    'file_size': [1, re.compile(r"")],
-    'operating_system': [1, re.compile(r"")]
+    'protocol': [1, re.compile(r"(?:TCP/IP|SMTP|PPP|FTP|SFTP|HTTPS?|TELNET|POP3|IPV[46]|ICMP|UDP|IMAP|SSH|Gopher)", re.IGNORECASE)],
+    'status_code': [1, re.compile(r"[1-5][0-9]{2}")],
+    'data_in': [1, re.compile(r"\d+[kmgtpezyKMGTPEZY][bB](?: |)in")],
+    'data_out': [1, re.compile(r"\d+[kmgtpezyKMGTPEZY][bB](?: |)out")],
+    'file_size': [1, re.compile(r"\d+[kmgtpezyKMGTPEZY][bB](?: |)")],
+    'operating_system': [1, re.compile(r"(?:windows|mac|linux|ubuntu|fedora|freebsd|android|solaris|ms-dos)", re.IGNORECASE)]
 }
 
 # Finds the index (character start and end positions) of all substrings matching the regex queried by user
